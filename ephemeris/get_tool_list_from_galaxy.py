1c2a4d3b15c9e7966a1022564ef1ebdc9553b200
Nitesh Turaga <nitesh.turaga@gmail.com>
2016-08-30 14:22:51 -0400

    tool_panel_section_name --> tool_panel_section_label
    as per https://github.com/galaxyproject/ansible-galaxy-tools/issues/35

--- ephemeris/get_tool_list_from_galaxy.py
+++ ephemeris/get_tool_list_from_galaxy.py
@@ -59,7 +59,7 @@ class GiToToolYaml:
                 'tool_shed_url': tsr['tool_shed'],
                 'revisions': [tsr['changeset_revision']],
                 'tool_panel_section_id': tool['panel_section_id'],
-                'tool_panel_section_name': tool['panel_section_name']}
+                'tool_panel_section_label': tool['panel_section_name']}
         return repo
 
     def get_repos_from_section(self, section):

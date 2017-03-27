3e9bf7cd2fead1b3cea5f91fbff059e1f57385a8
Björn Grüning <bjoern@gruenings.eu>
2017-03-27 14:18:47 +0200

    Support directories full of workflows

--- ephemeris/workflow_install.py
+++ ephemeris/workflow_install.py
@@ -5,12 +5,20 @@ import json
 from bioblend import galaxy
 
 
+def import_workflow(gi, path):
+    with open(path, 'r') as wf_file:
+        import_uuid = json.load(wf_file).get('uuid')
+    existing_uuids = [d.get('latest_workflow_uuid') for d in gi.workflows.get_workflows()]
+    if import_uuid not in existing_uuids:
+        gi.workflows.import_workflow_from_local_path(path)
+
 def main():
     """
         This script uses bioblend to import .ga workflow files into a running instance of Galaxy
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-w", "--workflow_path", help='Path to workflow file')
+    parser.add_argument("-w", "--workflow_path",
+                        help='Path to workflow file or a directory with multiple workflow files ending with ".ga"')
     parser.add_argument("-g", "--galaxy",
                         dest="galaxy_url",
                         help="Target Galaxy instance URL/IP address (required "
@@ -23,12 +31,13 @@ def main():
 
     gi = galaxy.GalaxyInstance(url=args.galaxy_url, key=args.api_key)
 
-    with open(args.workflow_path, 'r') as wf_file:
-        import_uuid = json.load(wf_file).get('uuid')
-    existing_uuids = [d.get('latest_workflow_uuid') for d in gi.workflows.get_workflows()]
-    if import_uuid not in existing_uuids:
-        gi.workflows.import_workflow_from_local_path(args.workflow_path)
-
+    if os.path.isdir(args.workflow_path) 
+        for file_path in os.listdir(args.workflow_path):
+            if file_path.endswith('.ga'):
+                import_workflow(gi, os.path.join(args.workflow_path, file_path))
+    else:
+        import_workflow(gi, args.workflow_path)
+    
 
 if __name__ == '__main__':
     main()

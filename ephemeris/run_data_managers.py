d6573ee1005468176f512e398a97069fc3d2b8d2
Bjoern Gruening <bjoern.gruening@gmail.com>
2017-04-05 13:27:31 +0200

    add new script to setup.py

--- ephemeris/run_data_managers.py
+++ ephemeris/run_data_managers.py
@@ -1,20 +1,21 @@
 ##!/usr/bin/env python
 
-import os
-import re
-import sys
-import time
 import argparse
-import urlparse
-import yaml
 import logging as log
+import re
+import time
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
+
+import yaml
 from bioblend.galaxy import GalaxyInstance
-from bioblend.galaxy.client import ConnectionError
 
 DEFAULT_URL = "http://localhost"
 
 
-def wait( gi, job ):
+def wait(gi, job):
     """
         Waits until a data_manager is finished or failed.
         It will check the state of the created datasets every 30s.
@@ -22,13 +23,13 @@ def wait( gi, job ):
     while True:
         value = job['outputs']
         # check if the output of the running job is either in 'ok' or 'error' state
-        if gi.datasets.show_dataset( value[0]['id'] )['state'] in ['ok', 'error']:
+        if gi.datasets.show_dataset(value[0]['id'])['state'] in ['ok', 'error']:
             break
         log.info('Data manager still running.')
         time.sleep(30)
 
 
-def main( args ):
+def main(args):
     url = args.galaxy or DEFAULT_URL
     gi = GalaxyInstance(url=url, email=args.user, password=args.password)
 
@@ -48,7 +49,6 @@ def main( args ):
                 key, value = param.items()[0]
                 value = re.sub(r'{{\s*item\s*}}', item, value, flags=re.IGNORECASE)
                 inputs.update({key: value})
-            print inputs
 
             # run the DM-job
             job = gi.tools.run_tool(history_id=None, tool_id=dm_id, tool_inputs=inputs)
@@ -57,16 +57,15 @@ def main( args ):
             for data_table in dm.get('data_table_reload', []):
                 # reload two times
                 for i in range(2):
-                    gi.make_get_request( urlparse.urljoin(url, 'api/tool_data/%s/reload' % data_table) )
+                    gi.make_get_request(urlparse.urljoin(url, 'api/tool_data/%s/reload' % data_table))
                     time.sleep(5)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Running Galaxy data managers in a defined order with defined parameters.'
-    )
+        description='Running Galaxy data managers in a defined order with defined parameters.')
     parser.add_argument("-v", "--verbose", help="Increase output verbosity.",
-                    action="store_true")
+                        action="store_true")
     parser.add_argument("--config", required=True, help="Path the YAML config file.")
     parser.add_argument("-g", "--galaxy",
                         help="Target Galaxy instance URL/IP address")
@@ -80,6 +79,4 @@ if __name__ == '__main__':
         log.basicConfig(level=log.DEBUG)
 
     log.info("Running data managers...")
-    main( args )
-
-
+    main(args)

ed0068bfc6322e84b52b373a686328a86c289d13
Bjoern Gruening <bjoern.gruening@gmail.com>
2017-04-17 00:41:00 +0200

    add echos to test script

--- test.sh
+++ test.sh
@@ -8,7 +8,11 @@ workflow-install --help
 setup-data-libraries --help
 get-tool-list --help
 
+echo "Check tool installation"
 shed-install -t tests/tool_list.yaml.sample --user admin@galaxy.org -p admin -g http://localhost:8080
+echo "Check workflow installation"
 workflow-install --user admin@galaxy.org -p admin -g http://localhost:8080 -w tests/test_workflow.ga 
+echo "Populate data libraries"
 setup-data-libraries --user admin@galaxy.org -p admin -g http://localhost:8080 -i tests/library_data_example.yaml
+echo "Get tool list from Galaxy"
 get-tool-list -o result_tool_list.yaml

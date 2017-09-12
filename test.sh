e8ecf4f05ea6497e87e2aa3d0b04b4f552651df8
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-09-12 09:20:39 +0200

    add test for already installed genomes

--- test.sh
+++ test.sh
@@ -24,4 +24,13 @@ echo "Check tool installation from workflow"
 shed-install -t result_workflow_to_tools.yaml -a admin -g http://localhost:8080
 shed-install -t result_workflow_to_tools.yaml --user admin@galaxy.org -p admin -g http://localhost:8080
 echo "Check installation of reference genomes"
-run-data-managers --user admin@galaxy.org -p admin -g http://localhost:8080 --config tests/run_data_managers.yaml.test -v
+run-data-managers --user admin@galaxy.org -p admin -g http://localhost:8080 --config tests/run_data_managers.yaml.test
+echo "Check if installation is skipped when reference genomes are already installed."
+run-data-managers --user admin@galaxy.org -p admin -g http://localhost:8080 --config tests/run_data_managers.yaml.test -v >> data_manager_output.txt
+# Check if already installed was thrown
+data_manager_already_installed = $(cat data_manager_output.txt | grep -i "already run for" | wc -l)
+if [ $data_manager_already_installed -ne 2 ]
+    then
+        echo "ERROR: Not all already installed genomes were skipped"
+        exit 1
+fi

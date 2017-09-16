5f48a4bf91eced3d579e8554d8ea261b7ecdfbe6
mvdbeek <m.vandenbeek@gmail.com>
2017-09-16 11:17:42 +0200

    Increase sleep after restart

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ echo "Check tool installation"
 shed-install -t tests/tool_list.yaml.sample -a admin -g http://localhost:8080
 #shed-install -t tests/tool_list.yaml.sample --user admin@galaxy.org -p admin -g http://localhost:8080
 #We restart galaxy because otherwise the data manager tables won't be watched
-docker exec galaxy_container supervisorctl restart galaxy: && sleep 30s
+docker exec galaxy_container supervisorctl restart galaxy: && sleep 60s
 echo "Check workflow installation"
 #workflow-install --user admin@galaxy.org -p admin -g http://localhost:8080 -w tests/test_workflow.ga
 workflow-install -a admin -g http://localhost:8080 -w tests/test_workflow.ga

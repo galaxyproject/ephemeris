159248b9ea4c3f62f8606db6b4a9cb8e9c1f3bca
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-09-18 15:43:09 +0200

    removed redundant verbose flags

--- tests/test.sh
+++ tests/test.sh
@@ -41,9 +41,9 @@ shed-install -t result_workflow_to_tools.yaml -a admin -g http://localhost:$WEB_
 #shed-install -t result_workflow_to_tools.yaml --user admin@galaxy.org -p admin -g http://localhost:$WEB_PORT
 echo "Check installation of reference genomes"
 #run-data-managers --user admin@galaxy.org -p admin -g http://localhost:$WEB_PORT --config ./run_data_managers.yaml.test -v
-run-data-managers -a admin -g http://localhost:$WEB_PORT --config "$TEST_DATA"/run_data_managers.yaml.test -v
+run-data-managers -a admin -g http://localhost:$WEB_PORT --config "$TEST_DATA"/run_data_managers.yaml.test
 echo "Check if installation is skipped when reference genomes are already installed."
-run-data-managers -a admin -g http://localhost:$WEB_PORT --config "$TEST_DATA"/run_data_managers.yaml.test -v >> data_manager_output.txt 2>&1
+run-data-managers -a admin -g http://localhost:$WEB_PORT --config "$TEST_DATA"/run_data_managers.yaml.test  >> data_manager_output.txt 2>&1
 # Check if already installed was thrown
 echo $(cat data_manager_output.txt | grep -i "already run for")
 data_manager_already_installed=$(cat data_manager_output.txt | grep -i "already run for" -c)

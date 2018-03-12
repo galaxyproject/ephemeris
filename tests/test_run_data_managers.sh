3e089f7ad4e2394fe93bf959f1cdfc6bce387bfa
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-03-12 10:24:46 +0100

    updated test scripts

--- tests/test_run_data_managers.sh
+++ tests/test_run_data_managers.sh
@@ -17,13 +17,13 @@ shed-tools install -t "$TEST_DATA"/data_manager_list.yaml -a admin -g http://loc
 # Test whether get-tool-list is able to fetch data managers
 echo "get-tool-list should not return data managers"
 get-tool-list -g http://localhost:$WEB_PORT -o result_tool_list_post.yaml
-grep -v data_manager_sam_fasta_index_builder result_tool_list_post.yaml
+grep -v data_manager result_tool_list_post.yaml
 echo "get-tool-list with an api key should not return data managers"
 get-tool-list -g http://localhost:$WEB_PORT -a admin -o result_tool_list_post.yaml
-grep -v data_manager_sam_fasta_index_builder result_tool_list_post.yaml
+grep -v data_manager result_tool_list_post.yaml
 echo "get-tool-list with an api_key and --get_data_mangers should return data managers"
 get-tool-list -g http://localhost:$WEB_PORT -a admin --get_data_managers -o result_tool_list_post.yaml
-grep data_manager_sam_fasta_index_builder result_tool_list_post.yaml
+grep data_manager result_tool_list_post.yaml
 
 echo "Wait a few seconds before restarting galaxy"
 sleep 15
@@ -38,6 +38,8 @@ galaxy-wait -g http://localhost:$WEB_PORT -v --timeout 120
 echo "Check installation of reference genomes"
 run-data-managers --user admin@galaxy.org -p admin -g http://localhost:$WEB_PORT --config "$TEST_DATA"/run_data_managers.yaml.test
 
+#TODO: Implement test whether the indexers where launched simeltaneously
+
 echo "Small waiting step to allow data-tables to update"
 # This seems to be necessary on travis
 sleep 15
@@ -47,8 +49,8 @@ run-data-managers -a admin -g http://localhost:$WEB_PORT --config "$TEST_DATA"/r
 # Check if already installed was thrown
 cat data_manager_output.txt
 
-echo "Number of skipped jobs should be 6"
-data_manager_already_installed=$(grep -i "Skipped jobs: 6" -c data_manager_output.txt)
+echo "Number of skipped jobs should be 9"
+data_manager_already_installed=$(grep -i "Skipped jobs: 9" -c data_manager_output.txt)
 if [ $data_manager_already_installed -ne 1 ]
     then
         echo "ERROR: Not all already installed genomes were skipped"

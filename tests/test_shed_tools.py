61e18bca6b51163568694c10e5aca217c9c50483
mvdbeek <m.vandenbeek@gmail.com>
2019-04-04 15:02:03 +0200

    Add test for tool testing

--- tests/test_shed_tools.py
+++ tests/test_shed_tools.py
@@ -3,8 +3,12 @@
 # for pytest to work.
 # pylint: disable=no-self-use,unused-import
 
+import json
 import logging
+import os
+import tempfile
 
+import pytest
 from docker_for_galaxy import start_container  # noqa: F401 prevent unused error
 
 from ephemeris.shed_tools import InstallRepositoryManager
@@ -27,3 +31,21 @@ class TestMiscellaneous(object):
                  sesame_ouvre_toi="Invalid key")
         ], log=logging.getLogger())
         assert "'sesame_ouvre_toi' not a valid key. Will be skipped during parsing" in caplog.text
+
+    @pytest.mark.parametrize("parallel_tests", [1, 2])
+    def test_tool_tests(self, caplog, start_container, parallel_tests):  # noqa: F811
+        container = start_container
+        irm = InstallRepositoryManager(container.gi)
+        caplog.set_level(logging.WARNING)
+        repos = [{'name': 'collection_element_identifiers', 'owner': 'iuc', 'tool_panel_section_label': "NGS: Alignment"}]
+        log = logging.getLogger()
+        irm.install_repositories(
+            repositories=repos,
+            log=log
+        )
+        fd, test_result_file = tempfile.mkstemp()
+        os.close(fd)
+        irm.test_tools(test_json=test_result_file, repositories=repos, log=log, parallel_tests=parallel_tests)
+        with open(test_result_file) as test_result:
+            result = json.load(test_result)
+        assert 'tests' in result

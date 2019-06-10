69d45081155299f0e3fb6787e782b80e2e5fe92e
Nolan Woods <nolan_w@sfu.ca>
2019-06-10 09:17:59 -0700

    Add yaml list support to install-tool-deps

--- src/ephemeris/install_tool_deps.py
+++ src/ephemeris/install_tool_deps.py
@@ -3,6 +3,7 @@
 import argparse
 import os
 import xml.etree.ElementTree as ET
+import yaml
 
 from bioblend.galaxy.tools import ToolClient
 
@@ -13,7 +14,7 @@ from ephemeris.common_parser import get_common_args
 def _parser():
     parent = get_common_args()
     parser = argparse.ArgumentParser(parents=[parent])
-    parser.add_argument("-t", "--tool", help='Path to a tool file or tool_conf file', nargs='*')
+    parser.add_argument("-t", "--tool", help='Path to a tool file, tool_conf file, or yaml file containing sequence of tool ids', nargs='*')
     parser.add_argument("-i", "--id", help='Comma seperated list of tool ids', nargs='*')
 
     return parser
@@ -28,19 +29,25 @@ def main():
     tool_client = ToolClient(gi)
 
     for tool_conf_path in args.tool: # type: str
-        # install all
-        root = ET.ElementTree(file=tool_conf_path).getroot()
-        if root.tag == "toolbox":
-            # Install all from tool_conf
-            tool_path = root.get('tool_path', '')
-            tool_path.replace('${tool_conf_dir}', os.path.abspath(args.tool))
-            for tool in root.findall("tool[@file]"):
-                tool_id = ET.ElementTree(file=tool.get('file')).getroot().get('id')
-                if tool_id:
-                    tool_client.install_dependencies(tool_id)
-        elif root.tag == "tool" and root.get('id'):
-            # Install from single tool file
-            tool_client.install_dependencies(root.get('id'))
+        _, ext = os.path.splitext(tool_conf_path)
+        if (ext == '.xml'):
+            # install all
+            root = ET.ElementTree(file=tool_conf_path).getroot()
+            if root.tag == "toolbox":
+                # Install all from tool_conf
+                tool_path = root.get('tool_path', '')
+                tool_path.replace('${tool_conf_dir}', os.path.abspath(args.tool))
+                for tool in root.findall("tool[@file]"):
+                    tool_id = ET.ElementTree(file=tool.get('file')).getroot().get('id')
+                    if tool_id:
+                        tool_client.install_dependencies(tool_id)
+            elif root.tag == "tool" and root.get('id'):
+                # Install from single tool file
+                tool_client.install_dependencies(root.get('id'))
+        else:
+            for tool_id in yaml.safe_load(tool_conf_path):
+                # Install from yaml file
+                tool_client.install_dependencies(tool_id)
 
     for tool_id in args.id:  # type: str
         tool_client.install_dependencies(tool_id.strip())

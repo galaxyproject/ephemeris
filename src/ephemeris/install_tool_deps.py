93fddbde2cf2953da2d068ae412aea4e3b1625f8
Nolan Woods <nolan_w@sfu.ca>
2019-08-06 16:26:39 -0700

    Handle argparse returning None for unspecified arguments

--- src/ephemeris/install_tool_deps.py
+++ src/ephemeris/install_tool_deps.py
@@ -28,29 +28,31 @@ def main():
     gi = get_galaxy_connection(args)
     tool_client = ToolClient(gi)
 
-    for tool_conf_path in args.tool:  # type: str
-        _, ext = os.path.splitext(tool_conf_path)
-        if (ext == '.xml'):
-            # install all
-            root = ET.ElementTree(file=tool_conf_path).getroot()
-            if root.tag == "toolbox":
-                # Install all from tool_conf
-                tool_path = root.get('tool_path', '')
-                tool_path.replace('${tool_conf_dir}', os.path.abspath(tool_conf_path))
-                for tool in root.findall("tool[@file]"):
-                    tool_id = ET.ElementTree(file=tool.get('file')).getroot().get('id')
-                    if tool_id:
-                        tool_client.install_dependencies(tool_id)
-            elif root.tag == "tool" and root.get('id'):
-                # Install from single tool file
-                tool_client.install_dependencies(root.get('id'))
-        else:
-            for tool_id in yaml.safe_load(tool_conf_path):
-                # Install from yaml file
-                tool_client.install_dependencies(tool_id)
+    if args.tool:
+        for tool_conf_path in args.tool:  # type: str
+            _, ext = os.path.splitext(tool_conf_path)
+            if (ext == '.xml'):
+                # install all
+                root = ET.ElementTree(file=tool_conf_path).getroot()
+                if root.tag == "toolbox":
+                    # Install all from tool_conf
+                    tool_path = root.get('tool_path', '')
+                    tool_path.replace('${tool_conf_dir}', os.path.abspath(tool_conf_path))
+                    for tool in root.findall("tool[@file]"):
+                        tool_id = ET.ElementTree(file=tool.get('file')).getroot().get('id')
+                        if tool_id:
+                            tool_client.install_dependencies(tool_id)
+                elif root.tag == "tool" and root.get('id'):
+                    # Install from single tool file
+                    tool_client.install_dependencies(root.get('id'))
+            else:
+                for tool_id in yaml.safe_load(tool_conf_path):
+                    # Install from yaml file
+                    tool_client.install_dependencies(tool_id)
 
-    for tool_id in args.id:  # type: str
-        tool_client.install_dependencies(tool_id.strip())
+    if args.id:
+        for tool_id in args.id:  # type: str
+            tool_client.install_dependencies(tool_id.strip())
 
 
 if __name__ == '__main__':

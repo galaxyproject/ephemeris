2538e3bacc3d51ceb9ba27d26a016ac9176d3932
Helena Rasche <hexylena@galaxians.org>
2022-11-28 15:10:47 +0100

    add requires, more recent versions that work

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setup(
     install_requires=requirements,
     license="AFL",
     zip_safe=False,
+    python_requires=">=3.7",
     keywords='galaxy',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -91,6 +92,8 @@ setup(
         "Programming Language :: Python :: 3",
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     test_suite=TEST_DIR,
     tests_require=test_requirements

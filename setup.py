867cf97ede05758a9cc32ce4977c2b9ea67e7441
Helena Rasche <hexylena@galaxians.org>
2022-11-28 14:42:41 +0100

    Drop 3.6 support

--- setup.py
+++ setup.py
@@ -89,7 +89,6 @@ setup(
         'Topic :: Software Development :: Testing',
         'Natural Language :: English',
         "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],

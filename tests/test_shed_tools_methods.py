ee9c8054232c2400a36a141877120020310f9cb1
mvdbeek <m.vandenbeek@gmail.com>
2023-02-13 17:00:47 +0100

    Run black and isort on tests/

--- tests/test_shed_tools_methods.py
+++ tests/test_shed_tools_methods.py
@@ -5,26 +5,31 @@ from ephemeris.shed_tools_methods import flatten_repo_info
 
 def test_flatten_repo_info():
     test_repositories = [
-        dict(name="bwa",
-             owner="devteam",
-             tool_panel_section_label="NGS: Alignment",
-             revisions=["1", "2"]),
-        dict(name="bowtie2",
-             owner="devteam",
-             tool_panel_section_label="NGS: Alignment",
-        )
+        dict(
+            name="bwa",
+            owner="devteam",
+            tool_panel_section_label="NGS: Alignment",
+            revisions=["1", "2"],
+        ),
+        dict(
+            name="bowtie2", owner="devteam", tool_panel_section_label="NGS: Alignment"
+        ),
     ]
     flattened_repos = flatten_repo_info(test_repositories)
-    assert (flattened_repos == [
-        dict(name="bwa",
-             owner="devteam",
-             tool_panel_section_label="NGS: Alignment",
-             changeset_revision="1"),
-        dict(name="bwa",
-             owner="devteam",
-             tool_panel_section_label="NGS: Alignment",
-             changeset_revision="2"),
-        dict(name="bowtie2",
-             owner="devteam",
-             tool_panel_section_label="NGS: Alignment")
-    ])
+    assert flattened_repos == [
+        dict(
+            name="bwa",
+            owner="devteam",
+            tool_panel_section_label="NGS: Alignment",
+            changeset_revision="1",
+        ),
+        dict(
+            name="bwa",
+            owner="devteam",
+            tool_panel_section_label="NGS: Alignment",
+            changeset_revision="2",
+        ),
+        dict(
+            name="bowtie2", owner="devteam", tool_panel_section_label="NGS: Alignment"
+        ),
+    ]

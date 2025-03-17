a301f81fd2d71695fbebc3537d877186c7d614e0
Matthias Bernt <m.bernt@ufz.de>
2025-03-17 17:56:38 +0100

    use verbose arbument more often

--- src/ephemeris/shed_tools.py
+++ src/ephemeris/shed_tools.py
@@ -673,7 +673,7 @@ def args_to_repos(args) -> list[InstallRepoDict]:
 def main(argv=None):
     disable_external_library_logging()
     args = parser().parse_args(argv)
-    log = setup_global_logger(name=__name__, log_file=args.log_file)
+    log = setup_global_logger(name=__name__, log_file=args.log_file, verbose=args.verbose)
     gi = get_galaxy_connection(args, file=args.tool_list_file, log=log, login_required=True)
     install_repository_manager = InstallRepositoryManager(gi)
 

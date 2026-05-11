236ba6ee7ba41979d2647fb45f2aa302fea5c6e1
Matthias Bernt <m.bernt@ufz.de>
2026-05-11 16:28:15 +0200

    black

--- src/ephemeris/install_container.py
+++ src/ephemeris/install_container.py
@@ -14,7 +14,7 @@ from typing import List
 
 from bioblend.galaxy import GalaxyInstance
 from bioblend.galaxy.tools import ToolClient
-from bioblend.galaxy.container_resolution  import ContainerResolutionClient
+from bioblend.galaxy.container_resolution import ContainerResolutionClient
 from galaxy.tool_util.version import parse_version
 from galaxy.util.tool_version import remove_version_from_guid
 
@@ -27,7 +27,7 @@ def get_tool_list(galaxy_instance: GalaxyInstance, include: List[str], exclude: 
     """
     tool_client = ToolClient(galaxy_instance)
     tools = tool_client.get_tools()
-    
+
     tool_versions = {}
     for tool in tools:
         tool_id = tool["id"]
@@ -36,7 +36,7 @@ def get_tool_list(galaxy_instance: GalaxyInstance, include: List[str], exclude: 
         if exclude and any([re.search(f, tool_id) for f in exclude]):
             continue
         tool_id = remove_version_from_guid(tool_id) or tool_id
-    
+
         if tool_id not in tool_versions:
             tool_versions[tool_id] = []
         try:
@@ -54,35 +54,38 @@ def get_tool_list(galaxy_instance: GalaxyInstance, include: List[str], exclude: 
             tool_list.append(t[1])
     return tool_list
 
-parser = argparse.ArgumentParser(description='List / install containers')
-parser.add_argument('--url', type=str, action='store', required=True, default=None, help='Galaxy URL')
+
+parser = argparse.ArgumentParser(description="List / install containers")
+parser.add_argument("--url", type=str, action="store", required=True, default=None, help="Galaxy URL")
 parser.add_argument(
     "--key", type=str, action="store", required=False, default=None, help="API key, better set API_KEY env var"
 )
 parser.add_argument(
-    '--include',
+    "--include",
     type=str,
-    action='append',
+    action="append",
     dest="include",
     default=[],
-    help='include tool id by searching for regexp, if any filter applies a tool is included'
+    help="include tool id by searching for regexp, if any filter applies a tool is included",
 )
 parser.add_argument(
-    '--exclude',
+    "--exclude",
     type=str,
-    action='append',
+    action="append",
     dest="exclude",
     default=[],
-    help='filter tool id by searching for regexp, if any filter applies a tool is excluded'
+    help="filter tool id by searching for regexp, if any filter applies a tool is excluded",
 )
-parser.add_argument('--latest', action='store_true', default=False, help='consider only the latest version of the tool')
-parser.add_argument('--install_container', action='store_true', default=False, help='install the container')
-parser.add_argument('--tabulate', type=int, action='store', required=False, default=None, help='resolver index')
-parser.add_argument( '-log',
-                     '--loglevel',
-                     choices=['debug', 'info', 'warning', 'error'],
-                     default='warning',
-                     help='Provide logging level. Example --loglevel debug, default=warning' )
+parser.add_argument("--latest", action="store_true", default=False, help="consider only the latest version of the tool")
+parser.add_argument("--install_container", action="store_true", default=False, help="install the container")
+parser.add_argument("--tabulate", type=int, action="store", required=False, default=None, help="resolver index")
+parser.add_argument(
+    "-log",
+    "--loglevel",
+    choices=["debug", "info", "warning", "error"],
+    default="warning",
+    help="Provide logging level. Example --loglevel debug, default=warning",
+)
 args = parser.parse_args()
 
 logging.getLogger().setLevel(logging.WARNING)
@@ -95,17 +98,17 @@ handler = logging.StreamHandler()
 logger.addHandler(handler)
 
 # Add a formatter to the handler (optional)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 
-key = os.environ.get('GALAXY_API_KEY', args.key)
+key = os.environ.get("GALAXY_API_KEY", args.key)
 galaxy_instance = GalaxyInstance(url=args.url, key=key)
 
 # get tools (matching filters and latest arguments)
 tool_list = get_tool_list(galaxy_instance, args.include, args.exclude, args.latest)
 
 new_containers = set()
-container_resolution_client = ContainerResolutionClient(galaxy_instance = galaxy_instance)
+container_resolution_client = ContainerResolutionClient(galaxy_instance=galaxy_instance)
 
 resolvers = container_resolution_client.get_container_resolvers()
 for i, resolver in enumerate(resolvers):
@@ -113,11 +116,11 @@ for i, resolver in enumerate(resolvers):
 
 for tool in tool_list:
     logger.debug(f"Checking {tool}")
-    res = container_resolution_client.resolve_toolbox(tool_ids = [tool])
+    res = container_resolution_client.resolve_toolbox(tool_ids=[tool])
     container = None
     for i, r in enumerate(res):
         logger.debug(f"{r=}")
-        tool_id = r['tool_id']
+        tool_id = r["tool_id"]
         container = r["status"].get("environment_path")
 
     if container is None:
@@ -125,22 +128,22 @@ for tool in tool_list:
         continue
 
     if args.tabulate:
-        res = container_resolution_client.resolve_toolbox(tool_ids = [tool], index=4)
+        res = container_resolution_client.resolve_toolbox(tool_ids=[tool], index=4)
         container_uri = None
         for i, r in enumerate(res):
             logger.debug(f"{r=}")
-            tool_id = r['tool_id']
+            tool_id = r["tool_id"]
             container_uri = r["status"].get("environment_path")
-        print(f"{tool}\t{container}\t{container_uri}")        
+        print(f"{tool}\t{container}\t{container_uri}")
 
     if args.install_container:
         if os.path.exists(container):
             logger.debug(f"Container for {tool} already installed {os.path.basename(container)}")
             continue
 
-        res = container_resolution_client.resolve_toolbox(tool_ids = [tool], install=args.install_container)
+        res = container_resolution_client.resolve_toolbox(tool_ids=[tool], install=args.install_container)
         for i, r in enumerate(res):
-            tool_id = r['tool_id']
+            tool_id = r["tool_id"]
             new_container = r["status"].get("environment_path")
 
             if new_container and os.path.exists(new_container):
